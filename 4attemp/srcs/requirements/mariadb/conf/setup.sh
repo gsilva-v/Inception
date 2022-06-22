18e892b1390198bedf3da44b05eb81108f8adbaa
gsilva-v <gaa.silva53@gmail.com>
2022-06-22 13:31:21 -0300

    envio

--- 4attemp/srcs/requirements/mariadb/conf/setup.sh
+++ 4attemp/srcs/requirements/mariadb/conf/setup.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
 set -x
-DB_PATH=/var/lib/mysql/Wordpress 
+
+DB_PATH=/var/lib/mysql/${DB_NAME} 
 if [ ! -d "$DB_PATH" ] 
 then
 	service mysql start
 
-	# mysql -uroot -pSenha321 -h localhost
+	# mysql -uroot -p${DB_PASS_ROOT} -h localhost
 	# This sql file will setup 2 database users (root and db_user)
-	mysql -e "CREATE DATABASE IF NOT EXISTS Wordpress;";
-	mysql -e "grant all on *.* to 'root'@'localhost' with grant option;";
-	mysql -e "create user 'root'@'%';";
-	mysql -e "grant all on *.* to 'root'@'%' with grant option;";
-	mysql -e "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('Senha321');";
-	mysql -e "SET PASSWORD FOR 'root'@'%'=PASSWORD('Senha321');";
+	mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};";
+	mysql -e "grant all on *.* to '"${DB_ROOT}"'@'localhost' with grant option;";
+	mysql -e "create user '"${DB_ROOT}"'@'%';";
+	mysql -e "grant all on *.* to '"${DB_ROOT}"'@'%' with grant option;";
+	mysql -e "SET PASSWORD FOR '"${DB_ROOT}"'@'localhost'=PASSWORD('"${DB_PASS_ROOT}"');";
+	mysql -e "SET PASSWORD FOR '"${DB_ROOT}"'@'%'=PASSWORD('"${DB_PASS_ROOT}"');";
 	mysql -e "FLUSH PRIVILEGES;";
 
-	mysql -uroot -pSenha321 -e  "CREATE USER 'gsilva'@'localhost' IDENTIFIED BY '123';";
-	mysql -uroot -pSenha321 -e "CREATE USER 'gsilva'@'%' IDENTIFIED BY '123';";
-	mysql -uroot -pSenha321 -e "GRANT ALL PRIVILEGES ON *.* TO 'gsilva'@'localhost';";
-	mysql -uroot -pSenha321 -e "GRANT ALL PRIVILEGES ON *.* TO 'gsilva'@'%';";
-	mysql -uroot -pSenha321 -e "FLUSH PRIVILEGES;";
+	mysql -u${DB_ROOT} -p${DB_PASS_ROOT} -e  "CREATE USER '"${DB_USER}"'@'localhost' IDENTIFIED BY '"${DB_PASS_USER}"';";
+	mysql -u${DB_ROOT} -p${DB_PASS_ROOT} -e "CREATE USER '"${DB_USER}"'@'%' IDENTIFIED BY '"${DB_PASS_USER}"';";
+	mysql -u${DB_ROOT} -p${DB_PASS_ROOT} -e "GRANT ALL PRIVILEGES ON *.* TO '"${DB_USER}"'@'localhost';";
+	mysql -u${DB_ROOT} -p${DB_PASS_ROOT} -e "GRANT ALL PRIVILEGES ON *.* TO '"${DB_USER}"'@'%';";
+	mysql -u${DB_ROOT} -p${DB_PASS_ROOT} -e "FLUSH PRIVILEGES;";
 
 
-	service mysql stop
-	# This sql file will setup 2 users for wp (wp_root and pcunha)
-	
 	# This file will allow access to the database from outside the container
-	# service mysql start && mysql -u$DB_ROOT -p$DB_PASS_ROOT -D$DB_NAME < /conf/wordpress.sql
-
-	mysqladmin -uroot -pSenha321 shutdown
+	# mysql -u$DB_ROOT -p$DB_PASS_ROOT -D$DB_NAME < /tmp/conf/dump01.sql
+	service mysql stop
+	mysqladmin -u${DB_ROOT} -p${DB_PASS_ROOT} shutdown
 fi
 
 exec "$@"
